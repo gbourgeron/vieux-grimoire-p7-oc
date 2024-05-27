6a65d02c628f7b391e15133ef2d798255b1d63d2
Gurvan B <gbourgeron@gmail.com>
2024-05-27 17:32:31 +0200

    FIRST GET route

--- app.js
+++ app.js
@@ -2,23 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("Requête reçue");
-    next();
+app.use('/api/books', (req, res, next) => {
+    const book = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Star Wars Episode IV: A New Hope',
+            author: 'George Lucas',
+            imageUrl: 'https://static.wikia.nocookie.net/starwars/images/3/3a/FromtheAdventuresofLukeSkywalker.png/revision/latest?cb=20190926173352',
+            year: 1976,
+            price: 4900,
+            genre: 'Sci-Fi',
+            rating: 4,
+            userId: 'gsomihvgios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Star Wars Episode V: The Empire Strikes Back',
+            author: 'George Lucas',
+            imageUrl: 'https://static.wikia.nocookie.net/starwars/images/8/80/Episodev_empirestrikesback.jpg/revision/latest?cb=20210409185847',
+            year: 1980,
+            price: 2900,
+            genre: 'Sci-Fi',
+            rating: 4,
+            userId: 'gsomihvgios',
+        },
+    ];
+    res.status(200).json(book);
 });
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({message: "Votre requête a bien été reçue"})
-    next();
-});
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès');
-})
 
 module.exports = app;
