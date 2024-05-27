901923392bd7452090363fb695a24281dbe426ff
Gurvan B <gbourgeron@gmail.com>
2024-05-27 17:47:54 +0200

    CORS ERROR fixed

--- app.js
+++ app.js
@@ -2,28 +2,45 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTION');
+    next();
+});
+
 app.use('/api/books', (req, res, next) => {
     const book = [
         {
             _id: 'oeihfzeoi',
             title: 'Star Wars Episode IV: A New Hope',
             author: 'George Lucas',
-            imageUrl: 'https://static.wikia.nocookie.net/starwars/images/3/3a/FromtheAdventuresofLukeSkywalker.png/revision/latest?cb=20190926173352',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/en/b/bc/StarWarsNovelization.jpg',
             year: 1976,
-            price: 4900,
-            genre: 'Sci-Fi',
-            rating: 4,
+            genre: 'Science-Fiction',
+            ratings: [
+                {
+                    userId: 'gsomihvgios',
+                    grade: 4,
+                }
+            ],
+            averageRating: 4,
             userId: 'gsomihvgios',
         },
         {
             _id: 'oeihfzeomoihi',
             title: 'Star Wars Episode V: The Empire Strikes Back',
             author: 'George Lucas',
-            imageUrl: 'https://static.wikia.nocookie.net/starwars/images/8/80/Episodev_empirestrikesback.jpg/revision/latest?cb=20210409185847',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/en/8/80/Episodev_empirestrikesback.jpg',
             year: 1980,
-            price: 2900,
-            genre: 'Sci-Fi',
-            rating: 4,
+            genre: 'Science-Fiction',
+            ratings: [
+                {
+                    userId: 'gsomihvgios',
+                    grade: 5,
+                }
+            ],
+            averageRating: 5,
             userId: 'gsomihvgios',
         },
     ];
