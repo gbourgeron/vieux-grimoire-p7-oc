a5c54dad33b2475e8748a823f0fbd4d75beda61c
Gurvan B <gbourgeron@gmail.com>
2024-05-28 15:15:12 +0200

    BOOK Schema created

---
+++ models/Book.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const ratingSchema = mongoose.Schema({
+    userId: { type: String, required: true},
+    grade: { type: Number, required: true}
+})
+
+const bookSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    year: { type: Number, required: true},
+    genre: { type: String, required: true},
+    ratings: { type: [ratingSchema], required: true},
+    averageRating: { type: Number, required: true}
+});
+
+module.exports = mongoose.model('Book', bookSchema);
