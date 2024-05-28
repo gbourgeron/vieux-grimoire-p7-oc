33f710b0d3975474aaccb99f8453dab92cb4d76a
Gurvan B <gbourgeron@gmail.com>
2024-05-28 14:59:11 +0200

    MONGODB connected with mongoose installed

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://gbourgeron:zEEqGZBKC6L4FoxR@cluster0.fyf4qd8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,7 +18,14 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/books', (req, res, next) => {
+app.post('/api/books', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Livre créé'
+    });
+})
+
+app.get('/api/books', (req, res, next) => {
     const book = [
         {
             _id: 'oeihfzeoi',
